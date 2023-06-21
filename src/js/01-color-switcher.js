a80034ab20d186cc63de45d3ff9b8d51ae0744fb
Domysaty <satyrkoo@gmail.com>
2023-06-21 21:27:37 +0200

    made 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+let intervalId = null;
+
+function onStartClick() {
+  startBtn.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function onStopClick() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
